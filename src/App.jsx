fb2adecfcd54a076cf65c0921868ce1870d90a3c
lucianocabelloo <you@example.com>
2023-12-20 07:26:11 -0300

    Add useState, function changeMessage, button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import Hello from './components/Hello'
 
 function App() {
 
-  const friend = "my friend of Rolling Code"
+  const [message, setMessage] = useState("")
+
+  const changeMessage = () => {
+    message 
+    ? setMessage("")
+    : setMessage("From changed state")
+  }
+
+  const friend = `my friend of Rolling Code ${message}`
 
   return (
     <>
       <Hello greetings={friend} />
+      <button onClick={changeMessage}> Click Me</button>
     </>
   )
 }
